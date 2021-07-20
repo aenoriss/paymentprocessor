dc29ea469b5e278f81c7ae9bdc640e6a5dce6a77
dariovp <dariovp01@gmail.com>
2021-07-20 18:51:13 -0300

    Paxful error

--- pages/api/pay.js
+++ pages/api/pay.js
@@ -3,7 +3,7 @@ import initMiddleware from '../../lib/init-middleware'
 import HmacSHA256 from 'crypto-js/sha256';
 import axios from "axios";
 
-export default async function handler(req, res) {
+export default async function pay(req, res) {
   // Run cors
   const cors = initMiddleware(
     // You can read more about the available options here: https://github.com/expressjs/cors#configuration-options
@@ -11,20 +11,27 @@ export default async function handler(req, res) {
       // Only allow requests with GET, POST and OPTIONS
       methods: ['GET', 'POST', 'OPTIONS'],
     })
-  )
+  );
 
-  await cors(req, res)
+  await cors(req, res);
 
-  const hash = HmacSHA256("merchant=onxQZvaVQZq&apikey=5cCuzYw1AOUKLFS69ruxRBC6qBAQWFBH&nonce=1386178459&to=1CkSCqyWGtVjok5A5xeGKKyMvpeZMnfEbq&fiat_amount=1", "HOSFvwa9pev8gMk3Cj6q6elKdwSkEPaL");
-    console.log("initiated", hash );
-    axios.get("http://paxful.com/wallet/pay", {
-      merchant:"onxQZvaVQZq", 
-      apikey:"e4sDZyCdWYBTcA6iuIR09ddgZCEOgEAq",
-      apiseal:hash,
-      nonce:1386178459,
-      to: "1CkSCqyWGtVjok5A5xeGKKyMvpeZMnfEbq",
-      amount: 1,
-    }).catch(function(err){
-      console.log("ERROR", err)
-    })
+  const merchantId = process.env.MERCHANT_ID;
+  const apiKey = process.env.API_KEY;
+
+  const hash = HmacSHA256(`merchant=${merchantId}&apikey=${apiKey}&nonce=${1386178459}&to=1CkSCqyWGtVjok5A5xeGKKyMvpeZMnfEbq&fiat_amount=1`);
+  // console.log("initiated", hash );
+  await axios.get("http://paxful.com/wallet/pay", {
+    merchant: merchantId, 
+    apikey: apiKey,
+    apiseal: hash,
+    nonce: 1386178459,
+    to: "1CkSCqyWGtVjok5A5xeGKKyMvpeZMnfEbq",
+    fiat_amount: 1,
+  }).then((response => {
+    console.log("Response: ", response);
+    res.status(200)
+  })).catch(function(err){
+    console.log("ERROR", err)
+    res.status(400)
+  })
 }
